cb1d78f88d0643feae2f3a194edfb74593f15d59
Dordovel <dordovel@gmail.com>
2021-01-23 13:53:14 +0300

    fix delay

--- src/delay_base.cpp
+++ src/delay_base.cpp
@@ -1,33 +1,58 @@
+#include <iostream>
 #include "../header/delay_base.hpp"
 
-
-ADelay::ADelay(float delay, bool stat):_frame(sf::seconds(delay)), _isRun(stat){}
+ADelay::ADelay(long delay, bool stat): _isRun(stat), _isPause(stat), _delay(delay)
+{
+    this->_isRun = true;
+    this->_thread = std::thread(&ADelay::worker, this, this);
+}
 
 bool ADelay::is_run() const noexcept
 {
-	return this->_isRun;
+	return this->_isPause;
 }
 
 void ADelay::run() noexcept
 {
-	this->_isRun = true;
+	this->_isPause = false;
+	this->_cv.notify_one();
+}
+
+void ADelay::worker(ADelay* delay) const noexcept
+{
+    std::mutex mt;
+    std::unique_lock lk(mt);
+
+    while(delay->_isRun)
+    {
+        if(!delay->_isPause)
+        {
+            delay->invoke();
+            std::this_thread::sleep_for(std::chrono::milliseconds (this->_delay));
+        }
+        else if(delay->_isPause)
+        {
+            delay->_cv.wait(lk);
+        }
+    }
 }
 
 void ADelay::stop() noexcept
 {
-	this->_isRun = false;
+	this->_isPause = true;
 }
 
-void ADelay::update(unsigned int delta) noexcept
+ADelay::~ADelay()
 {
-	if(this->_isRun)
-	{
-		this->_current += sf::milliseconds(delta);
-		if (this->_current >= this->_frame)
-		{
-			this->_current = sf::microseconds(this->_current.asMicroseconds() % this->_frame.asMicroseconds());
+    if(this->_isRun)
+        this->_isRun = false;
 
-			this->invoke();
-		}
-	}
+    if(this->_isPause)
+    {
+        this->_isPause = false;
+        this->_cv.notify_one();
+    }
+
+    if(this->_thread.joinable())
+        this->_thread.join();
 }
